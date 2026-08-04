ac0ab005e475d610d9998107da5a3d4e975daf27
cengizozel <60388555+cengizozel@users.noreply.github.com>
2026-08-04 14:30:50 +0300

    remove second monitor toggle bind

--- hypr/scripts/second-monitor-toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Toggle the second monitor output. Disabling migrates its workspaces to the
-# primary; re-enabling restores the layout from monitors.conf via reload.
-MON=HDMI-A-1
-if hyprctl monitors -j | jq -e --arg m "$MON" '.[] | select(.name==$m)' >/dev/null; then
-    hyprctl keyword monitor "$MON, disable"
-else
-    hyprctl reload
-fi
